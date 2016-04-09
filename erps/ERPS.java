b3e00c4a84e81d99f843f3569c42b2ca687d5bfc
iamsitting <cdsalamanca27@gmail.cok>
2016-04-09 14:12:15 -0500

    erps buffer check thread

--- erps/ERPS.java
+++ erps/ERPS.java
@@ -1,23 +1,50 @@
-import java.util.Random;
+import java.util.*;
+import java.io.*;
 
 public class ERPS {
-	public static void main(String[] args){
+	
+	//BTBuffer In Job
+	public class CheckBTBufferIn implements Runnable{
+		int buffval;
+		Scanner reader;
+		public CheckBTBufferIn(){
+			reader = new Scanner(System.in);
+		}
+		
+		public void run(){
+			try{
+				while(true){
+					System.out.println("Waiting for value: ");
+					buffval = reader.nextInt();
+					if(buffval == 1){
+						System.out.println("Enabled!");
+						break;
+					}
+				}
+				
+			}
+			catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public void go(){
 		int btbufferin = 0;
 		Random rand = new Random();
 		int enable = 0;
-		//check bt receiver
-		while (true){
-			btbufferin = rand.nextInt(10) + 1;
-			
-			if (btbufferin == 1){
-				enable = 1;
-				break;
-			}
-		}
-		
-		if(enable == 1){
-			System.out.println(btbufferin);
-			System.out.println("ERPS enabled");
-		}
+	
+		Runnable checkIn = new CheckBTBufferIn();
+		Thread t1 = new Thread(checkIn);
+		t1.start();
+		System.out.println("Buffer Checker Started");
 	}
+	
+	public static void main(String[] args){
+		ERPS erps = new ERPS();
+		erps.go();
+		
+	}
+	
+	
 }
